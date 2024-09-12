a1a90ffa8dc5e1a20773abd765b0ed13b8e8bb1c
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2024-09-12 12:38:09 +0530

    add change

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
+
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Node API app is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+module.exports = app;
