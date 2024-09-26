2023a542daed217fc195336522382914728a23b0
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2024-09-26 12:19:11 +0530

    add change

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -5,7 +5,7 @@ const {uploadToS3}=require('../comman/multerConfig');
 
 const router = express.Router()
 
-router.get('/get', getProperties)
+router.get('/get/:vendorId', getProperties)
 router.post('/post', uploadToS3, addProperty)
 router.put('/update/:id', uploadToS3, updateProperty)
 router.delete('/delete/:id', deleteProperty)
