02aeccd5f9830d7b1f8a8f39597be72a61bf5263
anurag721721 <anurag.yadav@aayaninfotech.com>
2024-11-25 16:47:15 +0530

    contact us

--- model/contactus.js
+++ model/contactus.js
@@ -14,6 +14,10 @@ const contactSchema = new mongoose.Schema(
         type: String,
         required: true,
     },
+        isRead: {
+        type: Boolean,
+        default:false
+    },
         createdAt: {
         type: Date,
         default: Date.now,
